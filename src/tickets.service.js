e693843a83b18815da4e989f5c23bbc08c30a0d1
IagoLast <iago.lastra@gmail.com>
2020-02-05 17:51:56 +0100

    feat: Add tickets tests

--- src/tickets.service.js
+++ src/tickets.service.js
@@ -1,5 +1,8 @@
 export function hasFreeTickets(user) {
-    
+    if (user.isVip) {
+        return true;
+    }
+    return false;
 }
 
 export default { hasFreeTickets };
--- src/tickets.service.spec.js
+++ src/tickets.service.spec.js
@@ -2,6 +2,16 @@ import ticketsService from "./tickets.service";
 
 describe('tickets.service', () => {
     describe('.hasFreeTickets(user)', () => {
-        
+        it('should give free tickets whe the user is VIP', () => {
+            const dummyUser = { isVip: true };
+            const actual = ticketsService.hasFreeTickets(dummyUser);
+            expect(actual).toBeTruthy();
+        });
+
+        it('should not give free tickets whe the user is VIP', () => {
+            const dummyUser = { isVip: false };
+            const actual = ticketsService.hasFreeTickets(dummyUser);
+            expect(actual).toBe(false);
+        });
     });
 });
