a07f8aaa468188fa42bd99f23ac7974dc40cdbb4
IagoLast <iago.lastra@gmail.com>
2020-02-16 12:01:49 +0100

    chore: Simpler specs

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,14 +1,11 @@
 /* global cy */
-describe('E2E: Free tickets', () => {
+it('an user should not have free tickets when is not VIP', () => {
+    cy.visit('localhost:3000');
+    cy.get('h2').should('have.text', '20€')
+});
 
-    it('should not have free tickets when the user is not VIP', () => {
-        cy.visit('localhost:3000');
-        cy.get('h2').should('have.text', '20€')
-    });
-
-    it('should have free tickets when the user is VIP', () => {
-        cy.visit('localhost:3000');
-        cy.get('#isVip').click();
-        cy.get('h2').should('have.text', '0€')
-    });
+it('an user should have free tickets when is VIP', () => {
+    cy.visit('localhost:3000');
+    cy.get('#isVip').click();
+    cy.get('h2').should('have.text', '0€')
 });
