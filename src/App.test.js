9c4d8836b76350dddeb65b9b7bb4da80d109fc45
IagoLast <iago.lastra@gmail.com>
2020-05-16 17:08:55 +0200

    Use userevent

--- src/App.test.js
+++ src/App.test.js
@@ -1,36 +1,40 @@
 
 // https://reactjs.org/docs/testing-recipes.html#data-fetching
-import { act, fireEvent, render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 import React from 'react';
 import apiClient from './api.client';
 import App from './AppHooks';
 
 
 describe('<App/>', () => {
-  let wrapper, $voucherInput;
+  let $voucherInput;
 
   beforeEach(() => {
     jest.spyOn(apiClient, 'checkVoucher').mockImplementation(fakeCheckVoucher);  // Replace the client with a fake implementation.
-    wrapper = render(<App />);
-    $voucherInput = wrapper.getByPlaceholderText('Código de descuento')
+    render(<App />);
+    $voucherInput = screen.getByPlaceholderText('Código de descuento')
   });
 
   it('should not have free tickets by default ', async () => {
-    expect(wrapper.queryByText('20€')).toBeVisible();
+    expect(screen.queryByText('20€')).toBeVisible();
   });
 
   it('should not have free tickets when the code is invalid', async () => {
-    await act(async () => fireEvent.change($voucherInput, { target: { value: 'invalid' } }));
-    expect(wrapper.queryByText('20€')).toBeVisible();
+    await act(() => userEvent.type($voucherInput, 'invalid'));
+    expect(screen.queryByText('20€')).toBeVisible();
   });
 
 
-  it('should have free tickets when the user insertas a valid voucher code', async () => {
-    await act(async () => fireEvent.change($voucherInput, { target: { value: 'valid' } }));
-    expect(wrapper.queryByText('0€')).toBeVisible();
+  it('should have free tickets when the user inserts a valid voucher code', async () => {
+    await act(() => userEvent.type($voucherInput, 'valid'));
+    expect(screen.queryByText('0€')).toBeVisible();
   });
 });
 
+/**
+ * Prevent false positives due server issues
+ */
 async function fakeCheckVoucher(code) {
   return code === 'valid';
 }
