f9b2c90bb2ef539d1c6c9cc5ddef56edd4b8a663
IagoLast <iago.lastra@gmail.com>
2020-08-25 13:38:22 +0200

    Fix base test

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-// import App from './AppComponent';
 import App from './AppHooks'; // This test will pass even when testing the same application written using hooks.
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({ adapter: new Adapter() });
 
 
 describe('<App/>', () => {
 
-  let wrapper;
-
   beforeEach(() => {
-    wrapper = render(<App />);
+    render(<App />);
   });
 
   describe('when the user is not VIP', () => {
     it('should not have right to free tickets ', () => {
-      expect(wrapper.queryByText('20€')).toBeVisible();
+      expect(screen.queryByText('20€')).toBeVisible();
     });
   });
 
   describe('when the user is VIP', () => {
     beforeEach(() => {
-      fireEvent.click(wrapper.getByTestId('isVip-checkbox'));
+      userEvent.click(screen.getByTestId('isVip-checkbox'));
     });
 
     it('should have right to free tickets ', () => {
-      expect(wrapper.queryByText('0€')).toBeVisible();
+      expect(screen.queryByText('0€')).toBeVisible();
     });
   });
 });
